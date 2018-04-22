aa01ac5002abeb766f478e6334921904f1cddd22
zeming <songzemings@gmail.com>
2018-04-22 17:24:14 +0800

    x

--- project/assets/resources/beWithYou/script/bwyHeroControl.js
+++ project/assets/resources/beWithYou/script/bwyHeroControl.js
@@ -49,12 +49,12 @@ cc.Class({
 
     onEnable: function () {
         cc.director.getCollisionManager().enabled = true;
-        cc.director.getCollisionManager().enabledDebugDraw = true;
+        // cc.director.getCollisionManager().enabledDebugDraw = true;
     },
 
     onDisable: function () {
         cc.director.getCollisionManager().enabled = false;
-        cc.director.getCollisionManager().enabledDebugDraw = false;
+        // cc.director.getCollisionManager().enabledDebugDraw = false;
     },
     
     onKeyPressed: function (keyCode, event) {
